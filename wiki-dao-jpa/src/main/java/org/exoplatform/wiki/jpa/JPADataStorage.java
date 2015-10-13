5e0005f0a50f6bf59d4c9d954aceb01f49ee45ff
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2015-10-13 16:00:41 +0200

    Reformat JPADataStorage class

--- wiki-dao-jpa/src/main/java/org/exoplatform/wiki/jpa/JPADataStorage.java
+++ wiki-dao-jpa/src/main/java/org/exoplatform/wiki/jpa/JPADataStorage.java
@@ -45,281 +45,281 @@ import org.exoplatform.wiki.service.search.WikiSearchData;
  */
 public class JPADataStorage implements DataStorage {
 
-    @Override
-    public PageList<SearchResult> search(WikiSearchData wikiSearchData) {
-        List<SearchResult> searchResults = new ArrayList<>();
-        Map<String, Collection<org.exoplatform.commons.api.search.data.SearchResult>> results;
-        SearchService searchService = PortalContainer.getInstance().getComponentInstanceOfType(SearchService.class);
+  @Override
+  public PageList<SearchResult> search(WikiSearchData wikiSearchData) {
+    List<SearchResult> searchResults = new ArrayList<>();
+    Map<String, Collection<org.exoplatform.commons.api.search.data.SearchResult>> results;
+    SearchService searchService = PortalContainer.getInstance().getComponentInstanceOfType(SearchService.class);
 
-        results = searchService.search(null, wikiSearchData.getTitle(), null, Collections.singleton("all"),
-                (int)wikiSearchData.getOffset(), wikiSearchData.getLimit(),
-                wikiSearchData.getSort(), wikiSearchData.getOrder());
-        for (String type : results.keySet()) {
-            for (org.exoplatform.commons.api.search.data.SearchResult result : results.get(type)) {
-                searchResults.add(toSearchResult(result));
-            }
-        }
-        return new ObjectPageList<>(searchResults, searchResults.size());
+    results = searchService.search(null, wikiSearchData.getTitle(), null, Collections.singleton("all"),
+            (int) wikiSearchData.getOffset(), wikiSearchData.getLimit(),
+            wikiSearchData.getSort(), wikiSearchData.getOrder());
+    for (String type : results.keySet()) {
+      for (org.exoplatform.commons.api.search.data.SearchResult result : results.get(type)) {
+        searchResults.add(toSearchResult(result));
+      }
     }
+    return new ObjectPageList<>(searchResults, searchResults.size());
+  }
 
-    private SearchResult toSearchResult(org.exoplatform.commons.api.search.data.SearchResult input) {
-        SearchResult output = new SearchResult();
-        output.setTitle(input.getTitle());
-        return output;
-    }
+  private SearchResult toSearchResult(org.exoplatform.commons.api.search.data.SearchResult input) {
+    SearchResult output = new SearchResult();
+    output.setTitle(input.getTitle());
+    return output;
+  }
 
-    @Override
-    public Wiki getWikiByTypeAndOwner(String s, String s1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Wiki getWikiByTypeAndOwner(String s, String s1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<Wiki> getWikisByType(String s) throws WikiException {
-        return null;
-    }
+  @Override
+  public List<Wiki> getWikisByType(String s) throws WikiException {
+    return null;
+  }
 
-    @Override
-    public Wiki createWiki(Wiki wiki) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Wiki createWiki(Wiki wiki) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page createPage(Wiki wiki, Page page, Page page1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page createPage(Wiki wiki, Page page, Page page1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getPageOfWikiByName(String s, String s1, String s2) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getPageOfWikiByName(String s, String s1, String s2) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getPageById(String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getPageById(String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getParentPageOf(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getParentPageOf(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<Page> getChildrenPageOf(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<Page> getChildrenPageOf(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void createTemplatePage(Wiki wiki, Template template) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void createTemplatePage(Wiki wiki, Template template) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void updateTemplatePage(Template template) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void updateTemplatePage(Template template) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deleteTemplatePage(String s, String s1, String s2) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deleteTemplatePage(String s, String s1, String s2) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deletePage(String s, String s1, String s2) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deletePage(String s, String s1, String s2) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Template getTemplatePage(WikiPageParams wikiPageParams, String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Template getTemplatePage(WikiPageParams wikiPageParams, String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Map<String, Template> getTemplates(WikiPageParams wikiPageParams) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Map<String, Template> getTemplates(WikiPageParams wikiPageParams) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deleteDraftOfPage(Page page, String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deleteDraftOfPage(Page page, String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deleteDraftByName(String s, String s1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deleteDraftByName(String s, String s1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void renamePage(String s, String s1, String s2, String s3, String s4) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void renamePage(String s, String s1, String s2, String s3, String s4) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void movePage(WikiPageParams wikiPageParams, WikiPageParams wikiPageParams1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void movePage(WikiPageParams wikiPageParams, WikiPageParams wikiPageParams1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<PermissionEntry> getWikiPermission(String s, String s1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<PermissionEntry> getWikiPermission(String s, String s1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void updateWikiPermission(String s, String s1, List<PermissionEntry> list) throws WikiException {
+  @Override
+  public void updateWikiPermission(String s, String s1, List<PermissionEntry> list) throws WikiException {
 
-    }
+  }
 
-    @Override
-    public List<Page> getRelatedPagesOfPage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<Page> getRelatedPagesOfPage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getRelatedPage(String s, String s1, String s2) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getRelatedPage(String s, String s1, String s2) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void addRelatedPage(Page page, Page page1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void addRelatedPage(Page page, Page page1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void removeRelatedPage(Page page, Page page1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void removeRelatedPage(Page page, Page page1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getExsitedOrNewDraftPageById(String s, String s1, String s2, String s3) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getExsitedOrNewDraftPageById(String s, String s1, String s2, String s3) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public DraftPage getDraft(WikiPageParams wikiPageParams, String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public DraftPage getDraft(WikiPageParams wikiPageParams, String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public DraftPage getLastestDraft(String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public DraftPage getLastestDraft(String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public DraftPage getDraft(String s, String s1) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public DraftPage getDraft(String s, String s1) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<DraftPage> getDraftPagesOfUser(String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<DraftPage> getDraftPagesOfUser(String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void createDraftPageForUser(DraftPage draftPage, String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void createDraftPageForUser(DraftPage draftPage, String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<TemplateSearchResult> searchTemplate(TemplateSearchData templateSearchData) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<TemplateSearchResult> searchTemplate(TemplateSearchData templateSearchData) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<SearchResult> searchRenamedPage(WikiSearchData wikiSearchData) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<SearchResult> searchRenamedPage(WikiSearchData wikiSearchData) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<Attachment> getAttachmentsOfPage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<Attachment> getAttachmentsOfPage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void addAttachmentToPage(Attachment attachment, Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void addAttachmentToPage(Attachment attachment, Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deleteAttachmentOfPage(String s, Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deleteAttachmentOfPage(String s, Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public Page getHelpSyntaxPage(String s, List<ValuesParam> list, ConfigurationManager configurationManager) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public Page getHelpSyntaxPage(String s, List<ValuesParam> list, ConfigurationManager configurationManager) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void createEmotionIcon(EmotionIcon emotionIcon) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void createEmotionIcon(EmotionIcon emotionIcon) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<EmotionIcon> getEmotionIcons() throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<EmotionIcon> getEmotionIcons() throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public EmotionIcon getEmotionIconByName(String s) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public EmotionIcon getEmotionIconByName(String s) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public String getPortalOwner() throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public String getPortalOwner() throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public boolean hasPermissionOnPage(Page page, PermissionType permissionType, Identity identity) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public boolean hasPermissionOnPage(Page page, PermissionType permissionType, Identity identity) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public boolean hasAdminSpacePermission(String s, String s1, Identity identity) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public boolean hasAdminSpacePermission(String s, String s1, Identity identity) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public boolean hasAdminPagePermission(String s, String s1, Identity identity) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public boolean hasAdminPagePermission(String s, String s1, Identity identity) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<PageVersion> getVersionsOfPage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<PageVersion> getVersionsOfPage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void addPageVersion(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void addPageVersion(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void restoreVersionOfPage(String s, Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void restoreVersionOfPage(String s, Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void updatePage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void updatePage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<String> getPreviousNamesOfPage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<String> getPreviousNamesOfPage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public List<String> getWatchersOfPage(Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public List<String> getWatchersOfPage(Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void addWatcherToPage(String s, Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void addWatcherToPage(String s, Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 
-    @Override
-    public void deleteWatcherOfPage(String s, Page page) throws WikiException {
-        throw new RuntimeException("Not implemented");
-    }
+  @Override
+  public void deleteWatcherOfPage(String s, Page page) throws WikiException {
+    throw new RuntimeException("Not implemented");
+  }
 }
