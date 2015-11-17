5f75250c00a1ea4c9c75600eec3ee209a7729b67
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2015-11-17 18:02:37 +0100

    Add listener on wiki pages to index them

---
+++ wiki-dao-jpa/src/main/java/org/exoplatform/wiki/jpa/search/PageIndexingListener.java
@@ -0,0 +1,31 @@
+package org.exoplatform.wiki.jpa.search;
+
+import org.exoplatform.addons.es.index.IndexingService;
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.wiki.WikiException;
+import org.exoplatform.wiki.mow.api.Page;
+import org.exoplatform.wiki.service.PageUpdateType;
+import org.exoplatform.wiki.service.listener.PageWikiListener;
+
+/**
+ * Listener on pages creation/update/deletion to index them
+ */
+public class PageIndexingListener extends PageWikiListener {
+  @Override
+  public void postAddPage(String wikiType, String wikiOwner, String pageId, Page page) throws WikiException {
+    IndexingService indexingService  = CommonsUtils.getService(IndexingService.class);
+    indexingService.index(WikiPageIndexingServiceConnector.TYPE, page.getId());
+  }
+
+  @Override
+  public void postUpdatePage(String wikiType, String wikiOwner, String pageId, Page page, PageUpdateType wikiUpdateType) throws WikiException {
+    IndexingService indexingService  = CommonsUtils.getService(IndexingService.class);
+    indexingService.reindex(WikiPageIndexingServiceConnector.TYPE, page.getId());
+  }
+
+  @Override
+  public void postDeletePage(String wikiType, String wikiOwner, String pageId, Page page) throws WikiException {
+    IndexingService indexingService  = CommonsUtils.getService(IndexingService.class);
+    indexingService.unindex(WikiPageIndexingServiceConnector.TYPE, page.getId());
+  }
+}
