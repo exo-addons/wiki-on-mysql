cc15b831652a86280cbd18b7e68ce51dd942bf2b
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2016-03-07 14:24:23 +0100

    Revert "Do not migrate users wikis that have not been modified" because it makes wiki deletions fail
    This reverts commit a9b14fa458dfd891789b6d6306ea82f8201d764b.

--- migration-services/src/main/java/org/exoplatform/wiki/jpa/migration/MigrationService.java
+++ migration-services/src/main/java/org/exoplatform/wiki/jpa/migration/MigrationService.java
@@ -367,29 +367,13 @@ public class MigrationService implements Startable {
             // get user wiki
             Wiki jcrWiki = jcrDataStorage.getWikiByTypeAndOwner(PortalConfig.USER_TYPE, user.getUserName());
 
-            // if it exists...
+            // if it exists, migrate it
             if(jcrWiki != null) {
               LOG.info("    Migration of the wiki of the user " + user.getUserName());
               if(jcrWiki.getOwner() == null) {
                 jcrWiki.setOwner(user.getUserName());
               }
-
-              Page jcrWikiHome = jcrWiki.getWikiHome();
-
-              List<PageVersion> jcrWikiHomeVersions = jcrDataStorage.getVersionsOfPage(jcrWikiHome);
-              boolean wikiUpdated = (jcrWikiHomeVersions != null && jcrWikiHomeVersions.size() > 1);
-              if(!wikiUpdated) {
-                List<Page> jcrWikiHomeChildren = jcrDataStorage.getChildrenPageOf(jcrWikiHome);
-                wikiUpdated = (jcrWikiHomeChildren != null && jcrWikiHomeChildren.size() > 0);
-              }
-
-              // ... and has been modified, migrate it
-              if(wikiUpdated) {
-                LOG.info("    Migration of the wiki of the user " + user.getUserName());
-                migrateWiki(jcrWiki);
-              } else {
-                LOG.info("    No need to migrate wiki of the user " + user.getUserName() + " since it has not been modified");
-              }
+              migrateWiki(jcrWiki);
             } else {
               LOG.info("    No wiki for user " + user.getUserName());
             }
